584050d51472cc9298e760f9cbfb214a01bbb253
mzeidler <mzeidler3@gmail.com>
2019-05-20 22:08:26 +0200

    sorting images

--- src/app/add-dog/add-dog.component.ts
+++ src/app/add-dog/add-dog.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { TranslationService } from '../translations/translation.service';
-import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MomentDateAdapter, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
 import { RestService } from '../service/rest.service';
 import { HttpEventType, HttpResponse } from '@angular/common/http';
 import { Image } from '../model/image';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-add-dog',
   templateUrl: './add-dog.component.html',
   styleUrls: ['./add-dog.component.css'],
   providers: [
-    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS}, 
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
   ],
 })
 export class AddDogComponent implements OnInit {
@@ -41,16 +42,43 @@ export class AddDogComponent implements OnInit {
 
   moveToLeft() {
 
+    if (this.data.dog.images.length > 1) {
+
+      for (var _i = 1; _i < this.data.dog.images.length; _i++) {
+        if (this.data.dog.images[_i].id == this.selectedId) {
+          let tmp = this.data.dog.images[_i].sortid;
+          this.data.dog.images[_i].sortid = this.data.dog.images[_i - 1].sortid;
+          this.data.dog.images[_i - 1].sortid = tmp;
+          break;
+        }
+      }
+
+      this.sortImages();  
+    }
+
   }
 
   moveToRight() {
+    
+    if (this.data.dog.images.length > 1) {
 
+      for (var _i = 0; _i < this.data.dog.images.length - 1; _i++) {
+        if (this.data.dog.images[_i].id == this.selectedId) {
+          let tmp = this.data.dog.images[_i].sortid;
+          this.data.dog.images[_i].sortid = this.data.dog.images[_i + 1].sortid;
+          this.data.dog.images[_i + 1].sortid = tmp;
+          break;
+        }
+      }
+
+      this.sortImages();
+    }
   }
 
   removePicture() {
     if (this.selectedId) {
       this.rest.deleteImage(this.selectedId);
-      this.data.dog.images = this.data.dog.images.filter(i => i.id != this.selectedId);  
+      this.data.dog.images = this.data.dog.images.filter(i => i.id != this.selectedId);
     }
   }
 
@@ -73,13 +101,15 @@ export class AddDogComponent implements OnInit {
         //image.url = this.rest.imageUrl + "/" + image.id;
         //this.images.push(image);
         this.data.dog.images.push(image);
-
-        this.data.dog.images.sort((a: Image, b: Image) => {
-          return b.sortid - a.sortid;
-        });
-
+        this.sortImages();
       }
     })
     this.progress.percentage = 0;
+  }
+
+  sortImages() {
+    this.data.dog.images.sort((a: Image, b: Image) => {
+      return b.sortid - a.sortid;
+    });
   }
 }
